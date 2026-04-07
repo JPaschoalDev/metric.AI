a9a8acd4322d5f92cf166664f6a646dee44a4bd0
João Paschoal <joaov.paschoal.75@gmail.com>
2026-04-07 18:00:44 -0300

    Initial backend structure

---
+++ src/main/java/com/metricai/api/MetricAiApiApplication.java
@@ -0,0 +1,13 @@
+package com.metricai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricAiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricAiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metricai/api/MetricAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metricai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricAiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
