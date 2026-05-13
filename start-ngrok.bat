c080c3db699f958a28b096579557e43184369596
João Paschoal <joaov.paschoal.75@gmail.com>
2026-05-13 14:30:27 -0300

    Mudanças de segurança com o MELI

---
+++ start-ngrok.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ================================
+echo    Iniciando Ngrok - Metric.AI
+echo ================================
+"C:\Program Files (x86)\ngrok\ngrok.exe" http --domain=untidy-estimate-direness.ngrok-free.dev 8080
